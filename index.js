09e743c1614658721e7dc2bce591bb33a3fdcb74
davay <davay@defucc.me>
2025-05-08 00:15:35 +0700

    feat: storage added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import Gun from 'gun';
+import Gun from "gun/gun";
 import "gun/lib/then";
 import "gun/lib/radix";
 import "gun/lib/radisk";
+import "gun/lib/store";
 import "gun/lib/rindexed";
 import "gun/lib/webrtc";
 
 import { default as SEA } from 'gun/sea.js';
 
-export { Gun, SEA };
+export { Gun, SEA }
